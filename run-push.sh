11a8ee1da1fd4f2710ff09522ac72abef64c0ab8
osboxes.org <tudor.ionescu@tuwien.ac.at>
2019-04-16 05:42:46 -0400

    updating scripts

--- run-push.sh
+++ run-push.sh
@@ -3,4 +3,6 @@
 git config --global user.email "tudor.ionescu@tuwien.ac.at"
 git remote set-url origin https://comemak:Kafka321!@github.com/CoMeMak/data
 
-watch -n1 './push-coords.sh'
+while true; do
+	./push-coords.sh
+done
